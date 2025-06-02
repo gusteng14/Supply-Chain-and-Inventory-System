90734e6e6f7d8fc7b07ec0477feaf1f5d610c61b
gusteng14 <gusteng14@gmail.com>
2025-06-03 05:20:28 +0800

    added addtl changes

--- src/main/java/com/barkstore/Barkstore/Controller/ContentController.java
+++ src/main/java/com/barkstore/Barkstore/Controller/ContentController.java
@@ -25,6 +25,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -74,7 +75,7 @@ public class ContentController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard() throws FileNotFoundException {
         return "dashboard";
     }
 
